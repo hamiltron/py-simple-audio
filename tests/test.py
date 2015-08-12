b9223d6d20931aa00d7798e6ce75ce287d44a6bf
Joe Hamilton <jhamilton10@georgefox.edu>
2015-08-11 18:05:44 -0700

    added new audio files, updated tests

--- tests/test.py
+++ tests/test.py
@@ -8,8 +8,16 @@ import time
 AUDIO_DIR = "test_audio"
 
 class TestSimpleaudio(unittest.TestCase):
-    
-    def test_simple(self):
-        with wave.open(os.path.join(AUDIO_DIR, "butts.wav"), 'rb') as wave_read:
+    def _test_file(self, file):
+        with wave.open(os.path.join(AUDIO_DIR, file), 'rb') as wave_read:
             sa.play_wave_read(wave_read)
             time.sleep(4)
+
+    def test_2_16_32(self):
+        self._test_file("butts_2_16_32.wav")
+
+    def test_2_16_44(self):
+        self._test_file("butts_2_16_44.wav")
+
+    def test_2_16_48(self):
+        self._test_file("butts_2_16_48.wav")
