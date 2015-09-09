f715fbac091acf84fc551916efa70080315538a2
Joe Hamilton <jhamilton10@georgefox.edu>
2015-09-08 18:09:29 -0700

    added WaitDoneTest

--- simpleaudio/functionchecks.py
+++ simpleaudio/functionchecks.py
@@ -13,7 +13,7 @@ def _gwp(wave_file):
 
 def run_all(countdown=3):
     checks = [LeftRightCheck, OverlappingCheck, RatesAndChannelsCheck,
-              StopCheck, StopAllCheck, IsPlayingCheck]
+              StopCheck, StopAllCheck, IsPlayingCheck, WaitDoneCheck]
     for check in checks:
         check.run(countdown)
 
@@ -44,8 +44,8 @@ class FunctionCheckBase(object):
 class LeftRightCheck(FunctionCheckBase):
     """
     This checks stereo playback by first playing a note
-    in the left channel only, then a different note in the 
-    right channel only. 
+    in the left channel only, then a different note in the
+    right channel only.
     """
 
     @classmethod
@@ -57,7 +57,7 @@ class LeftRightCheck(FunctionCheckBase):
 class OverlappingCheck(FunctionCheckBase):
     """
     This checks overlapped playback by playing three different notes
-    spaced approximately a half-second apart but still overlapping. 
+    spaced approximately a half-second apart but still overlapping.
     """
 
     @classmethod
@@ -93,9 +93,9 @@ class RatesAndChannelsCheck(FunctionCheckBase):
 
 class StopCheck(FunctionCheckBase):
     """
-    This checks stopping playback by playing three different 
+    This checks stopping playback by playing three different
     notes simultaneously and stopping two after approximately a half-second,
-    leaving only one note playing for two more seconds. 
+    leaving only one note playing for two more seconds.
     """
 
     @classmethod
@@ -114,8 +114,8 @@ class StopCheck(FunctionCheckBase):
 class StopAllCheck(FunctionCheckBase):
     """
     This checks stopping playback of all audio by playing three different
-    notes simultaneously and stopping all of them after approximately 
-    a half-second. 
+    notes simultaneously and stopping all of them after approximately
+    a half-second.
     """
 
     @classmethod
@@ -132,10 +132,10 @@ class StopAllCheck(FunctionCheckBase):
 
 class IsPlayingCheck(FunctionCheckBase):
     """
-    This checks functionality of the is_playing() method by 
-    calling during playback (when it should return True) 
+    This checks functionality of the is_playing() method by
+    calling during playback (when it should return True)
     and calling it again after all playback has stopped
-    (when it should return False). The output is printed. 
+    (when it should return False). The output is printed.
     """
 
     @classmethod
@@ -146,3 +146,17 @@ class IsPlayingCheck(FunctionCheckBase):
         print("Is playing:", play.is_playing())
         sleep(4)
         print("Is playing:", play.is_playing())
+
+class WaitDoneCheck(FunctionCheckBase):
+    """
+    This checks functionality of the wait_done() method
+    by using it to allow the three-note clip to play
+    until finished (before attempting to stop playback).
+    """
+
+    @classmethod
+    def check(cls):
+        wave = _gwp("notes_2_16_44.wav")
+        play = sa.play_buffer(*wave)
+        play.wait_done()
+        play.stop()
