fc841e517ae968deb33b4b75779536c7b4158329
Joe Hamilton <jhamilton10@georgefox.edu>
2015-09-08 17:55:52 -0700

    new test audio

--- simpleaudio/functionchecks.py
+++ simpleaudio/functionchecks.py
@@ -12,7 +12,7 @@ def _gwp(wave_file):
         return sa.get_wave_params(wave_read)
 
 def run_all(countdown=3):
-    checks = [LeftRightCheck, OverlappingCheck, RatesAndChannelsCheck, 
+    checks = [LeftRightCheck, OverlappingCheck, RatesAndChannelsCheck,
               StopCheck, StopAllCheck, IsPlayingCheck]
     for check in checks:
         check.run(countdown)
@@ -21,7 +21,7 @@ class FunctionCheckBase(object):
     @classmethod
     def check(cls):
         raise NotImplementedError()
-        
+
     @classmethod
     def run(cls, countdown=0):
         # print function check header
@@ -30,7 +30,7 @@ class FunctionCheckBase(object):
         print("--", cls.__name__, "--")
         print(cls.__doc__.strip())
         print("=" * 80)
-        
+
         if countdown > 0:
             print("Starting check in ...")
             for tick in reversed(range(1, countdown + 1)):
@@ -39,43 +39,45 @@ class FunctionCheckBase(object):
         print("RUNNING CHECK ...")
         cls.check()
         print("... DONE")
-        
+
 
 class LeftRightCheck(FunctionCheckBase):
     """
     This checks stereo playback by ...
     """
-        
+
     @classmethod
     def check(cls):
-        wave = _gwp("butts_2_16_44.wav")
+        wave = _gwp("left_right.wav")
         sa.play_buffer(*wave)
         sleep(4)
-        
+
 class OverlappingCheck(FunctionCheckBase):
     """
     This checks overlapped playback by ...
     """
-        
+
     @classmethod
     def check(cls):
-        wave_1 = _gwp("butts_2_16_44.wav")
-        wave_2 = _gwp("butts_2_16_44.wav")
-        wave_3 = _gwp("butts_2_16_44.wav")
+        wave_1 = _gwp("c.wav")
+        wave_2 = _gwp("e.wav")
+        wave_3 = _gwp("g.wav")
         sa.play_buffer(*wave_1)
+        sleep(0.5)
         sa.play_buffer(*wave_2)
+        sleep(0.5)
         sa.play_buffer(*wave_3)
-        sleep(4)
-    
+        sleep(3)
+
 class RatesAndChannelsCheck(FunctionCheckBase):
     """
-    This checks placback of mono and stereo audio at all allowed sample rates.
+    This checks placback of mono and stereo audio at all allowed sample rates and bit-depths.
     """
-    
-    waves = [("butts_2_16_32.wav", 2, 32000),
-             ("butts_2_16_44.wav", 2, 44100),
-             ("butts_2_16_48.wav", 2, 48000)]
-        
+
+    waves = [("notes_2_16_32.wav", 2, 32000),
+             ("notes_2_16_44.wav", 2, 44100),
+             ("notes_2_16_48.wav", 2, 48000)]
+
     @classmethod
     def check(cls):
         for wave_file, num_chan, sample_rate in cls.waves:
@@ -90,45 +92,45 @@ class StopCheck(FunctionCheckBase):
     """
     This checks stopping playback by ...
     """
-        
+
     @classmethod
     def check(cls):
-        wave_1 = _gwp("butts_2_16_44.wav")
-        wave_2 = _gwp("butts_2_16_44.wav")
-        wave_3 = _gwp("butts_2_16_44.wav")
+        wave_1 = _gwp("c.wav")
+        wave_2 = _gwp("e.wav")
+        wave_3 = _gwp("g.wav")
         play_1 = sa.play_buffer(*wave_1)
         play_2 = sa.play_buffer(*wave_2)
         play_3 = sa.play_buffer(*wave_3)
-        sleep(0.5)
+        sleep(0.25)
         play_1.stop()
         play_3.stop()
-        sleep(3.5)
-    
+        sleep(3)
+
 class StopAllCheck(FunctionCheckBase):
     """
     This checks stopping playback of all audio by ...
     """
-        
+
     @classmethod
     def check(cls):
-        wave_1 = _gwp("butts_2_16_44.wav")
-        wave_2 = _gwp("butts_2_16_44.wav")
-        wave_3 = _gwp("butts_2_16_44.wav")
+        wave_1 = _gwp("c.wav")
+        wave_2 = _gwp("e.wav")
+        wave_3 = _gwp("g.wav")
         sa.play_buffer(*wave_1)
         sa.play_buffer(*wave_2)
         sa.play_buffer(*wave_3)
         sleep(0.5)
         sa.stop_all()
-        sleep(3.5)
+        sleep(3)
 
 class IsPlayingCheck(FunctionCheckBase):
     """
     This checks stopping playback of all audio by ...
     """
-        
+
     @classmethod
     def check(cls):
-        wave = _gwp("butts_2_16_44.wav")
+        wave = _gwp("notes_2_16_44.wav")
         play = sa.play_buffer(*wave)
         sleep(0.5)
         print("Is playing:", play.is_playing())
