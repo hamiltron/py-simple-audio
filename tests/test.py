c6913984b0ee451b0ed87f3c73e43b9a239d63bf
Joe Hamilton <jhamilton10@georgefox.edu>
2015-09-23 17:38:28 -0700

    pep8

--- tests/test.py
+++ tests/test.py
@@ -4,11 +4,12 @@ import simpleaudio.functionchecks as fc
 import wave
 import unittest
 
+
 class TestSimpleaudio(unittest.TestCase):
-        
+
     def test_function_checks(self):
         fc.run_all(0)
-        
+
     def test_num_channels(self):
         self.assertRaises(ValueError, sa.play_buffer, b'\0'*16, 0, 2, 44100)
         self.assertRaises(ValueError, sa.play_buffer, b'\0'*16, 3, 2, 44100)
@@ -16,6 +17,6 @@ class TestSimpleaudio(unittest.TestCase):
     def test_bytes_per_chan(self):
         self.assertRaises(ValueError, sa.play_buffer, b'\0'*16, 2, 0, 44100)
         self.assertRaises(ValueError, sa.play_buffer, b'\0'*16, 2, 4, 44100)
-        
+
     def test_sample_rate(self):
         self.assertRaises(ValueError, sa.play_buffer, b'\0'*16, 2, 2, 44101)
