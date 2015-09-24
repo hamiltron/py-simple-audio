19d3509a8f4457aca328bd03325272dcbd2951c3
Joe Hamilton <jhamilton10@georgefox.edu>
2015-09-23 17:35:43 -0700

    pep8

--- simpleaudio/shiny.py
+++ simpleaudio/shiny.py
@@ -6,15 +6,18 @@ import _simpleaudio as _sa
 from time import sleep
 import wave
 
+
 class WaveObject(object):
-    def __init__(self, audio_data, num_channels=2, bytes_per_sample=2, sample_rate=44100):
+    def __init__(self, audio_data, num_channels=2, bytes_per_sample=2,
+                 sample_rate=44100):
         self.audio_data = audio_data
         self.num_channels = num_channels
         self.bytes_per_sample = bytes_per_sample
         self.sample_rate = sample_rate
 
     def play(self):
-        return play_buffer(self.audio_data, self.num_channels, self.bytes_per_sample, self.sample_rate)
+        return play_buffer(self.audio_data, self.num_channels,
+                           self.bytes_per_sample, self.sample_rate)
 
     @classmethod
     def from_wave_file(cls, wave_file):
@@ -23,11 +26,13 @@ class WaveObject(object):
 
     @classmethod
     def from_wave_read(cls, wave_read):
-        return cls(wave_read.readframes(wave_read.getnframes()), wave_read.getnchannels(),
-                   wave_read.getsampwidth(), wave_read.getframerate())
+        return cls(wave_read.readframes(wave_read.getnframes()),
+                   wave_read.getnchannels(), wave_read.getsampwidth(),
+                   wave_read.getframerate())
 
     def __str__(self):
-        return "Wave Object: {} channel, {} bit, {} Hz".format(self.num_channels, self.bytes_per_sample * 8, self.sample_rate)
+        return "Wave Object: {} channel, {} bit, {} Hz".format(
+            self.num_channels, self.bytes_per_sample * 8, self.sample_rate)
 
 
 class PlayObject(object):
@@ -44,10 +49,12 @@ class PlayObject(object):
     def is_playing(self):
         return _sa._is_playing(self.play_id)
 
+
 def stop_all():
     _sa._stop_all()
 
-def play_buffer(audio_data, num_channels, bytes_per_sample, sample_rate):
-    play_id = _sa._play_buffer(audio_data, num_channels, bytes_per_sample, sample_rate)
-    return PlayObject(play_id)
 
+def play_buffer(audio_data, num_channels, bytes_per_sample, sample_rate):
+    play_id = _sa._play_buffer(audio_data, num_channels, bytes_per_sample,
+                               sample_rate)
+    return PlayObject(play_id)
