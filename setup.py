66776310633b7d649c04a897830b1df77fbc11f3
Joe Hamilton <jhamilton10@georgefox.edu>
2017-05-07 07:47:35 -0700

    more version string

--- setup.py
+++ setup.py
@@ -43,7 +43,10 @@ tag_commit = str(latest_tag.commit)
 
 VERSION = str(latest_tag)
 if latest_commit != tag_commit:
-    VERSION = VERSION + "+" + latest_commit[:6]
+    untagged = list(
+        repo.iter_commits("{}..{}".format(tag_commit, latest_commit)))
+    commit_count = str(len(untagged))
+    VERSION = VERSION + "." + commit_count
 
 # Get the long description from the relevant file
 with open(path.join(path.abspath(path.dirname(__file__)), 'README.rst'),
