a985ceed7cb47cb3d1b7028f5991d52317d549bf
Joe Hamilton <jhamilton10@georgefox.edu>
2017-11-23 11:18:48 -0800

    test python bin path

--- .travis/osx_setup.sh
+++ .travis/osx_setup.sh
@@ -6,8 +6,11 @@ set -x
 curl https://www.python.org/ftp/python/${PY_VER}/python-${PY_VER}-macosx10.6.pkg -o pyinstall.pkg
 sudo installer -package pyinstall.pkg -target /
 
+which python3
+which pip
+which pip3
 
-#python -V
+python3 -V
 #pip -V
 #
 #pip install --upgrade pip
