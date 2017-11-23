8e1072900ab86639ada1def6ad6a4d5ffc6c5731
Joe Hamilton <jhamilton10@georgefox.edu>
2017-11-23 11:06:40 -0800

    trying installer

--- .travis/osx_setup.sh
+++ .travis/osx_setup.sh
@@ -3,7 +3,9 @@
 set -e
 set -x
 
-curl -O https://www.python.org/ftp/python/${PY_VER}/python-${PY_VER}-macosx10.6.pkg
+curl https://www.python.org/ftp/python/${PY_VER}/python-${PY_VER}-macosx10.6.pkg -o pyinstall.pkg
+installer -package pyinstall.pkg -target /
+
 
 #python -V
 #pip -V
