92f16b0d5b8a4364662661e1eec78c86a1054c2f
Joe Hamilton <jhamilton10@georgefox.edu>
2017-11-25 11:45:29 -0800

    reorg and consolidation

--- .travis/osx_install_py3.sh
+++ .travis/osx_install_py3.sh
@@ -5,11 +5,3 @@ set -x
 
 curl https://www.python.org/ftp/python/${PY_VER}/python-${PY_VER}-macosx10.6.pkg -o pyinstall.pkg
 sudo installer -package pyinstall.pkg -target /
-
-which python3
-which pip3
-python3 -V
-pip3 -V
-
-pip3 install --upgrade pip
-pip3 install --upgrade wheel setuptools twine
