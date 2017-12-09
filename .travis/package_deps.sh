fc918f7aa18d248287d744fc2a1c274194bcce24
Joe Hamilton <jhamilton10@georgefox.edu>
2017-12-09 10:14:14 -0800

    see path

--- .travis/package_deps.sh
+++ .travis/package_deps.sh
@@ -10,3 +10,7 @@ pip3 -V
 
 pip3 install --upgrade pip
 pip3 install --upgrade wheel setuptools twine
+
+echo "----------------------------------"
+echo $PATH
+echo "----------------------------------"
