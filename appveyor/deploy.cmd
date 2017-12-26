4409238b8d10dc6f4cc70ac7314681443320395a
Joe Hamilton <jhamilton10@georgefox.edu>
2017-12-25 20:33:03 -0800

    testing out deploy

--- appveyor/deploy.cmd
+++ appveyor/deploy.cmd
@@ -4,6 +4,7 @@ ECHO "deploy script..."
 
 IF "%APPVEYOR_REPO_TAG%"=="true" (
   ECHO "Tagged commit - attempting deploy"
+  twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 ) ELSE (
   ECHO "No tag, skipping deploy"
 )
