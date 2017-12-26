a9a1c6d3d0dfff347ad27c6cb68a6fd655d9b246
Joe Hamilton <jhamilton10@georgefox.edu>
2017-12-25 20:40:45 -0800

    see vars

--- appveyor/deploy.cmd
+++ appveyor/deploy.cmd
@@ -2,8 +2,12 @@
 
 ECHO "deploy script..."
 
+ECHO "%APPVEYOR_REPO_BRANCH%"
+ECHO "%APPVEYOR_REPO_TAG%"
+
+:: this is ugly - wish AV would have the deploy script switches that Travis does
 IF "%APPVEYOR_REPO_TAG%"=="true" (
-  ECHO "Tagged commit - attempting deploy"
+  ECHO "Tagged commit"
   twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 ) ELSE (
   ECHO "No tag, skipping deploy"
